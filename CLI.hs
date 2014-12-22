c57c631a7b253c852884f83a1a6233a43a36241a
Ayokunle <adeosua@tcd.ie>
2014-12-22 14:18:28 +0000

    added flag and command line interface

---
+++ CLI.hs
@@ -0,0 +1,121 @@
+import Minesweeper
+import Control.Monad
+import qualified Data.Map as M
+import qualified Data.Set as S
+import Data.Map(Map)
+import Data.Char
+ 
+showBoard :: GameState -> IO ()
+showBoard gs = do  -- Print col names
+  putStr "   "
+  putStrLn . unwords $ map show [1..w]
+ 
+  -- Print rows
+  printRow 1 h gs
+ 
+  where
+    (w,h) = getBounds gs
+    printRow n t gs =
+      if (n > t) then do
+        return ()
+      else do
+        putStr ((show n) ++ " :")
+        putStrLn . unwords $ rowHelper (rowSquares n gs)
+        printRow (n+1) t gs
+ 
+ 
+      where
+        rowSquares n gs = M.filterWithKey  (\(x,y) s -> y == n) (getBoard gs)
+        cellHelper Flagged      = "F"
+        cellHelper (Adjacent n) = show n
+        cellHelper _            = " "
+        rowHelper squares = [cellHelper s|(_,s) <- (M.toList squares)]
+ 
+showInternal :: GameState -> IO ()
+showInternal gs = do  -- Print col names
+  putStr "   "
+  putStrLn . unwords $ map show [1..w]
+ 
+  -- Print rows
+  printRow 1 h gs
+ 
+  where
+    (w,h) = getBounds gs
+    printRow n t gs =
+      if (n > t) then do
+        return ()
+      else do
+        putStr ((show n) ++ " :")
+        putStrLn . unwords $ rowHelper (rowSquares n gs)
+        printRow (n+1) t gs
+ 
+ 
+      where
+        rowSquares n gs = M.filterWithKey  (\(x,y) s -> y == n) (getInternal gs)
+        cellHelper Mine      = "M"
+        cellHelper (IAdjacent n) = show n
+        rowHelper squares = [cellHelper s|(_,s) <- (M.toList squares)]
+ 
+prompt :: GameState -> IO ()
+prompt gs = do
+  putStrLn "Enter your move (f = flag, c = click, q = quit)"
+  line <- getLine
+  case (dropWhile isSpace $ takeWhile (not . isSpace) line) of -- checks for space and retu
+    "f" -> do
+      putStrLn "Enter the coordinate of the square you wish to flag"
+      line <- getLine
+      let pos = read line :: (Int, Int)
+ 
+      case (makeMove (Move Flag pos) gs) of
+        (Left Win) -> do
+          putStrLn "You won!"
+        (Left Lose) -> do
+          putStrLn "You lost!"
+        (Right gs')  -> do
+          showBoard gs'
+          showInternal gs'
+          prompt gs'
+    "c" -> do
+      putStrLn "Enter the coordinate of the square you wish to click"
+      line <- getLine
+      let pos = read line :: (Int, Int)
+ 
+      case (makeMove (Move Click pos) gs) of
+        (Left Win) -> do
+          putStrLn "You won!"
+        (Left Lose) -> do
+          putStrLn "You lost!"
+        (Right gs')  -> do
+          showBoard gs'
+          prompt gs'
+    "q" -> do
+      return ()
+    _ -> do
+      prompt gs
+ 
+ 
+ 
+main :: IO ()
+main = do
+  putStrLn "Please enter the board width"
+  line <- getLine 
+  let w = read line :: Int
+ 
+  putStrLn "Please enter the board height"
+  line <- getLine
+  let h = read line :: Int
+ 
+  putStrLn "Please enter the number of mines"
+  line <- getLine
+  let n = read line :: Int
+ 
+  putStr "\n\n"
+ 
+  if (w < 0 || h < 0 || (w*h) < n) then
+    putStrLn "The data you entered was invalid, please try again\n" >> main
+  else do
+    gs <- (generateBoard (w,h) n)
+    showBoard gs
+    showInternal gs
+    prompt gs
+    return ()
